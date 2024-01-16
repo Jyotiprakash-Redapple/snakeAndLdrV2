e11f506f269e888500837fbef1d0bae81e53f532
hellochiku <hello@chiku.com>
2024-01-16 10:50:25 +0530

    update_turn socket event fixed

--- src/page/game/player/Player.jsx
+++ src/page/game/player/Player.jsx
@@ -10,13 +10,7 @@ import { FaHeartBroken } from "react-icons/fa";
 import Popupbox from "../../../components/popup/popupbox";
 import { snakes, ladder, generateArea, cordinate } from "../../../arbitar/helper";
 
-import {
-	makeNewMove,
-	dectateWin,
-	makeAnimateMove,
-	animateDice,
-	currentDice,
-} from "../../../arbitar/context/reducer/move";
+import { makeNewMove, dectateWin, makeAnimateMove, animateDice, currentDice } from "../../../arbitar/context/reducer/move";
 
 import ReactDice from "react-dice-complete";
 import PawnMovementSound from "../../../audio/pawn_movement.mp3";
@@ -232,37 +226,38 @@ function PlayWithPlayer() {
 	}
 
 	return (
-		<main className="game_body">
-			<div className="gameplay_box">
-				<div className="game_bg">
-					<img src="/asset/game_play/bg.png" alt="" />
-					<div className="top-area">
-						<div className="time">{moment.utc(appState?.gameTime * 1000).format("mm:ss")} </div>
-						<div className="top-control">
-							<div className="back">
-								<img src="/asset/game_play/back.png" alt="back" onClick={() => setQuitGame(true)} />
+		<main className='game_body'>
+			<div className='gameplay_box'>
+				<div className='game_bg'>
+					<img src='/asset/game_play/bg.png' alt='' />
+					<div className='top-area'>
+						<div className='time'>{moment.utc(appState?.gameTime * 1000).format("mm:ss")} </div>
+						<div className='top-control'>
+							<div className='back'>
+								<img src='/asset/game_play/back.png' alt='back' onClick={() => setQuitGame(true)} />
 							</div>
-							<div className="sound">
-								{soundStatus && (
-									<img
-										src="/asset/game_play/sound_on.png"
-										alt="sound"
-										onClick={() => setSoundStatus(!soundStatus)}
-									/>
-								)}
-								{!soundStatus && (
-									<img
-										src="/asset/game_play/sound_off.png"
-										alt="sound"
-										onClick={() => setSoundStatus(!soundStatus)}
-									/>
-								)}
+							<div className='sound'>
+								{soundStatus && <img src='/asset/game_play/sound_on.png' alt='sound' onClick={() => setSoundStatus(!soundStatus)} />}
+								{!soundStatus && <img src='/asset/game_play/sound_off.png' alt='sound' onClick={() => setSoundStatus(!soundStatus)} />}
 							</div>
 						</div>
 					</div>
-					<div className="player-info-area">
-						<div className="left">
-							<div className="player-name player-name-left">{appState.pl?.user_name}</div>
+					<div className='player-info-area'>
+						<div className='left'>
+							<div className='player-name player-name-left'>
+								<p
+									style={{
+										color: "#fff",
+										whiteSpace: "nowrap",
+										width: "60%",
+										overflow: "hidden",
+										textAlign: "center",
+										textOverflow: "ellipsis",
+									}}>
+									{" "}
+									{appState.pl?.user_name}
+								</p>
+							</div>
 							{appState?.pl.id === appState.turnTime.current_player_id && appState.turnTime.life ? (
 								<div
 									style={{
@@ -279,13 +274,13 @@ function PlayWithPlayer() {
 									) : (
 										<IoMdHeart style={{ color: "#ED5AB3", fontSize: "20px" }} />
 									)}
-									<div className="turn_bar_bg">
-										<div className="turn_bar_inActive_bg">
+									<div className='turn_bar_bg'>
+										<div className='turn_bar_inActive_bg'>
 											<img
-												src="/asset/game_play/time_bar.png"
+												src='/asset/game_play/time_bar.png'
 												width={20}
 												height={30}
-												alt="loader"
+												alt='loader'
 												style={{
 													position: "absolute",
 													left: "0",
@@ -302,15 +297,15 @@ function PlayWithPlayer() {
 							) : (
 								<></>
 							)}
-							<div className="user-image user-left">
-								<img src={appState?.pl?.profile || "/default.png"} alt="" />
+							<div className='user-image user-left'>
+								<img src={appState?.pl?.profile || "/default.png"} alt='' />
 							</div>
-							<img src="/asset/game_play/Left_base.png" alt="" />
+							<img src='/asset/game_play/Left_base.png' alt='' />
 						</div>
-						<div className="center">
-							<img src="/asset/game_play/dise_base.png" alt="" />
+						<div className='center'>
+							<img src='/asset/game_play/dise_base.png' alt='' />
 							<div
-								className="dice_movement"
+								className='dice_movement'
 								style={{
 									width: "100%",
 									height: "70%",
@@ -324,19 +319,28 @@ function PlayWithPlayer() {
 									justifyContent: "center",
 								}}>
 								{appState.diceAnimate ? (
-									<img src="/asset/game_play/dice_gif.gif" alt="dice"></img>
+									<img src='/asset/game_play/dice_gif.gif' alt='dice'></img>
 								) : (
-									<img
-										src={`/asset/game_play/${appState.diceValue || 1}.png`}
-										alt=""
-										style={{ objectFit: "contain", width: "70%", height: "70%", paddingBottom: "10px" }}
-									/>
+									<img src={`/asset/game_play/${appState.diceValue || 1}.png`} alt='' style={{ objectFit: "contain", width: "70%", height: "70%", paddingBottom: "10px" }} />
 								)}
 								{/* dice rule component*/}
 							</div>
 						</div>
-						<div className="right">
-							<div className="player-name player-name-right">{appState.op?.user_name}</div>
+						<div className='right'>
+							<div className='player-name player-name-right'>
+								{" "}
+								<p
+									style={{
+										color: "#fff",
+										whiteSpace: "nowrap",
+										width: "60%",
+										overflow: "hidden",
+										textAlign: "center",
+										textOverflow: "ellipsis",
+									}}>
+									{appState.op?.user_name}
+								</p>
+							</div>
 							{appState.op.id === appState.turnTime.current_player_id && appState.turnTime.life ? (
 								<div
 									style={{
@@ -353,13 +357,13 @@ function PlayWithPlayer() {
 									) : (
 										<IoMdHeart style={{ color: "#ED5AB3", fontSize: "20px" }} />
 									)}
-									<div className="turn_bar_bg">
-										<div className="turn_bar_inActive_bg">
+									<div className='turn_bar_bg'>
+										<div className='turn_bar_inActive_bg'>
 											<img
-												src="/asset/game_play/time_bar.png"
+												src='/asset/game_play/time_bar.png'
 												width={20}
 												height={30}
-												alt="loader"
+												alt='loader'
 												style={{
 													position: "absolute",
 													left: "0",
@@ -377,58 +381,43 @@ function PlayWithPlayer() {
 							) : (
 								""
 							)}
-							<div className="user-image user-right">
-								<img src={appState?.op?.profile || "/default.png"} alt="" />
+							<div className='user-image user-right'>
+								<img src={appState?.op?.profile || "/default.png"} alt='' />
 							</div>
-							<img src="/asset/game_play/right_base.png" alt="" />
+							<img src='/asset/game_play/right_base.png' alt='' />
 						</div>
 					</div>
-					<div className="gameplay-board">
+					<div className='gameplay-board'>
 						{quitGame && (
-							<div className="quit_game_bg">
-								<div className="quit_game_wrapper">
-									<div className="quit_game_text">Do You Want To Quit ?</div>
-									<div className="quit_game_btn">
+							<div className='quit_game_bg'>
+								<div className='quit_game_wrapper'>
+									<div className='quit_game_text'>Do You Want To Quit ?</div>
+									<div className='quit_game_btn'>
 										{" "}
-										<button className="yes" onClick={() => handelQuitGame()}>
+										<button className='yes' onClick={() => handelQuitGame()}>
 											Yes
 										</button>
-										<button className="no" onClick={() => setQuitGame(false)}>
+										<button className='no' onClick={() => setQuitGame(false)}>
 											No
 										</button>
 									</div>
 								</div>
 							</div>
 						)}
-						<Board
-							playerPositions={appState.position}
-							turn={appState.turn}
-							animate={appState.diceAnimate || animate}
-						/>
+						<Board playerPositions={appState.position} turn={appState.turn} animate={appState.diceAnimate || animate} />
 						<Popupbox />
 					</div>
 					<div
-						className="roll-btn"
+						className='roll-btn'
 						style={{
 							cursor: "pointer",
 
 							pointerEvents:
-								appState.position?.player1?.color === appState?.turn &&
-								!animate &&
-								appState.status !== gameStatus.r_win &&
-								appState.status !== gameStatus.y_win
-									? "auto"
-									: "none",
+								appState.position?.player1?.color === appState?.turn && !animate && appState.status !== gameStatus.r_win && appState.status !== gameStatus.y_win ? "auto" : "none",
 						}}
 						onClick={rollDice}>
-						{appState.position?.player1?.color === appState?.turn && !animate ? (
-							<img src="/asset/game_play/Roll.png" alt="" />
-						) : (
-							<></>
-						)}
-						<p>
-							{appState.position?.player1?.color === appState?.turn && !animate ? "Your Turn" : "Opponent Turn"}
-						</p>
+						{appState.position?.player1?.color === appState?.turn && !animate ? <img src='/asset/game_play/Roll.png' alt='' /> : <></>}
+						<p>{appState.position?.player1?.color === appState?.turn && !animate ? "Your Turn" : "Opponent Turn"}</p>
 					</div>
 				</div>
 			</div>
